51f5133fe55642c4da7905d31e8341a4674d7190
Darya <darsolyar@icloud.com>
2021-08-25 17:59:12 +0300

    Routes skeleton done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const cardsRouter = require('./routes/cards');
+const usersRouter = require('./routes/users');
+const cartRouter = require('./routes/cart');
 
 const app = express();
 
@@ -16,9 +19,15 @@ app.use(express.json());
 // Static
 app.use(express.static(path.join(__dirname, 'static')));
 
+
 app.get('/', (req, res) => {
   res.redirect('/cards');
 });
+
+// Routes
+app.use('/cards', cardsRouter);
+app.use('/users', usersRouter);
+app.use('/cart', cartRouter);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
