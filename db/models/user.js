3d6a0c833bea1301fa0a5171f097df7532438f27
Serghey Stoyanov <82656183+Sstoy@users.noreply.github.com>
2021-08-25 16:58:26 +0300

    DB Release 3 - minimal migrations with associations created

--- db/models/user.js
+++ db/models/user.js
@@ -9,8 +9,8 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ UserCard }) {
+      User.hasMany(UserCard);
     }
   }
   User.init({
--- db/models/user_card.js
+++ db/models/user_card.js
@@ -9,16 +9,29 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ User, Card }) {
+      UserCard.belongsTo(User);
+      UserCard.belongsTo(Card);
     }
   }
   UserCard.init({
     cardId: {
       type: DataTypes.INTEGER,
+      references: {
+        model: 'Card',
+        key: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
     },
     userId: {
       type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
     },
     status: {
       type: DataTypes.TEXT,
