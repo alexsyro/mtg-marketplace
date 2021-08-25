208e316082ede6b0742577e78b957aa6ab971d01
Darya <darsolyar@icloud.com>
2021-08-25 18:02:07 +0300

    Routes skeleton

---
+++ routes/cards.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/new', (req, res) => {
+  
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/:id', (req, res) => {
+  
+});
+
+router.get('/:id/profile', (req, res) => {
+
+});
+
+module.exports = router;
