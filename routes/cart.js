4d1045180b21d1464d392d76ba3aed286dcd662e
Serghey Stoyanov <82656183+Sstoy@users.noreply.github.com>
2021-08-26 14:58:01 +0300

    Cart-Release 0 - init

--- routes/cart.js
+++ routes/cart.js
@@ -6,4 +6,11 @@ router.get('/', (req, res) => {
   res.render('cart/index');
 });
 
+router.put('/', (req, res) => {
+  const cart = [];
+  const addCard = UserCard.find
+  res.cookie('cart', cart);
+
+});
+
 module.exports = router;
