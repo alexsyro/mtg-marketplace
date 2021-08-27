5509b7965a2b450e3d587fd4c3220a9a4863f620
Serghey Stoyanov <82656183+Sstoy@users.noreply.github.com>
2021-08-27 14:40:41 +0300

    Cart fixed

--- routes/cart.js
+++ routes/cart.js
@@ -5,10 +5,39 @@ const { UserCard } = require('../db/models');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  const { cart } = req.session;
-  console.log(cart);
-  res.render('cart/index', { session: req.session });
+router.get('/', async (req, res) => {
+  if (req.session.isAutorized) {
+  // console.log(req.session.cart);
+    const { cart } = req.session;
+    // console.log(JSON.parse(cart));
+    const cardsPromises = cart.map(async (el) => {
+      const card = await UserCard.findOne({
+        where: {
+          id: Number(el),
+        },
+      });
+      return card;
+    });
+    const cards = await Promise.all(cardsPromises);
+    // console.log('!!!!!!!', cards);
+    res.render('cart/index', { cards });
+  } else if (req.cookies.cart) {
+    // console.log(req.session.cart);
+    const cart = JSON.parse(req.cookies.cart);
+    const cardsPromises = cart.map(async (el) => {
+      const card = await UserCard.findOne({
+        where: {
+          id: Number(el),
+        },
+      });
+      return card;
+    });
+    const cards = await Promise.all(cardsPromises);
+    // console.log('!!!!!!!', cards);
+    res.render('cart/index', { cards });
+  } else {
+    res.render('cart/index');
+  }
 });
 
 router.put('/', async (req, res) => {
@@ -19,7 +48,8 @@ router.put('/', async (req, res) => {
     if (!req.session.cart) {
       req.session.cart = [];
       req.session.cart.push(userCardId);
-
+      res.status(200).send({ number: req.session.cart.length });
+    } else if (req.session.cart.find((el) => el === userCardId) !== -1) {
       res.status(200).send({ number: req.session.cart.length });
     } else {
       req.session.cart.push(userCardId);
@@ -29,17 +59,22 @@ router.put('/', async (req, res) => {
   } else if (!allCookies.cart) {
     const arrOfCart = [];
     arrOfCart.push(userCardId);
-    console.log(arrOfCart);
+    // console.log(arrOfCart);
     // console.log('arrOfCart', JSON.stringify(arrOfCart));
     res.cookie('cart', JSON.stringify(arrOfCart));
     res.status(200).send({ number: arrOfCart.length });
   } else {
     const arrOfCart = await JSON.parse(allCookies.cart);
-    // console.log('arrOfCart!!!!!!!!!!!!!!', arrOfCart);
-    arrOfCart.push(userCardId);
-    // console.log('arrOfCart', JSON.stringify(arrOfCart));
-    res.cookie('cart', JSON.stringify(arrOfCart));
-    res.status(200).send({ number: arrOfCart.length });
+    if (arrOfCart.find((el) => el === userCardId) !== -1) {
+      res.cookie('cart', JSON.stringify(arrOfCart));
+      res.status(200).send({ number: arrOfCart.length });
+    } else {
+      // console.log('arrOfCart!!!!!!!!!!!!!!', arrOfCart);
+      arrOfCart.push(userCardId);
+      // console.log('arrOfCart', JSON.stringify(arrOfCart));
+      res.cookie('cart', JSON.stringify(arrOfCart));
+      res.status(200).send({ number: arrOfCart.length });
+    }
   }
 });
 module.exports = router;
